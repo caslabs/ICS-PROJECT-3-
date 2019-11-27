e81cbf8ba83290176693d8f885d07313b4ca9666
Jeraldy <jeraldy@kuure.com>
2019-11-26 19:24:15 -1000

    assets folder with team logo

--- src/main.java
+++ src/main.java
@@ -2,6 +2,16 @@
 public class main {
 
 	public static void main(String[] args) {
+		/*
+		TODO : create auto window mode, custom resolution, boot menu?
+		 */
+
+		//Testing width and height for Jeraldy's computer. You can change it for yours.
+		int width = 1080;
+		int height = 720;
+		EZ.initialize(width, height);
+		EZ.addImage("assets/smile.png",width/2, height/2);
+
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Logan was here");
