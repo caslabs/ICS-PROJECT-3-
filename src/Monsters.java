ad5b5d1d94e57611031a6f19e03566feb382c18e
loganm01 <loganjamoniz@gmail.com>
2019-12-08 01:41:15 -1000

    added a bunch

---
+++ src/Monsters.java
@@ -0,0 +1,12 @@
+
+public class Monsters extends Characters{
+
+	public Monsters(String filename, int posx, int posy) {
+		super(filename, posx, posy);
+		
+	}
+	void die() {
+		
+	}
+	
+}
--- src/main.java
+++ src/main.java
@@ -1,46 +1,56 @@
 import java.awt.Color;
+import java.io.*;
 import java.util.*;
 
 public class main {
 	
-	//A Stack that will hold all the maps
-//	static Stack<Integer> maps =new Stack<Integer>();
-//	static int currMap = maps.peek();
+	static int score;
+	static boolean gameRunning = true;
 	
-	
-	/*need a funct 
-	 that chooses random numbers 
-	 puts said numbers in the stack for the order of the maps
-	*/
+	//Array lists keys
+	static ArrayList<key> keys = new ArrayList<key>();
 	
 	//start up 
 	static void startUp() {
-		Screen screen = new Screen(900,900);
+		Screen screen = new Screen(500,281);
 		EZ.setBackgroundColor(new Color(0, 0, 0));
-	}
-	
-	//The loading screen 
-	static void loadScreen() {
-		EZ.setBackgroundColor(new Color(0, 0, 0));
-		EZ.refreshScreen();
-	}
-	
-	//sets the map
-	static void mapSet(int i) {
-		EZ.setBackgroundColor(new Color(0, 0, 0));
-		EZ.refreshScreen();
+		EZ.addImage("background.png",250, 140);
 	}
 	
 	//Function that runs when player completes a level
-	static void Escape() {
-		maps.pop();
-		loadScreen();
-		currMap = maps.peek();
-		mapSet(currMap);				
+	static void Escape() {		
 	}
-	public static void main(String[] args) {
-		
+	
+	// win function
+	static void win() {
+		if (score == 3) {
+			
+		}
+	}
+	
+	public static void main(String[] args) throws java.io.IOException{
 		startUp();
+		
+		// file reading for the positions of the keys
+		FileReader keyRead = new FileReader("key_pos.txt");
+		Scanner keyPos = new Scanner(keyRead);
+		int posx, posy;
+				
+		for(int i =0; i < 3; i++) {
+			posx = keyPos.nextInt();
+			posy = keyPos.nextInt();
+			keys.add(new key("key.png",posx,posy));
+		}
+		keyPos.close();
+		
+		//create Player
+		Characters logan = new Characters("logan_walk1.png",10, 230);
+		
+		while(gameRunning) {
+			logan.move();
+			EZ.refreshScreen();
+			
+		}
 	}
 
 }
